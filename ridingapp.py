c2703f36c0ea1b65b930d5af4aad882fe08b9b19
ani67h <ghoshanindya4589@gmail.com>
2025-06-12 19:08:32 +0600

    uthao

---
+++ ridingapp.py
@@ -0,0 +1,92 @@
+print("Welcome everyone to Uthao! In this app we kidnap people. If you are bored with your life, then you can select a car and take a ride, in order to get kidnapped. If you have any further queries email it to our CEO Ms. Humayra Mehzabin Sejuti.")
+print("\n""\n""Select a vehicle :- ")
+print("\n""1. Car")
+print("\n""2. Bike")
+print("\n""3. Boat")
+print("\n""4. Helicopter")
+
+choice = int(input("\n""\n""Enter the number of vehicle that you have chosen : "))
+
+if choice == 1:
+    print("\n""\n""Select a type of car :- ")
+    print("\n""1. SUV")
+    print("\n""2. Crossover")
+    print("\n""3. Coupe")
+    print("\n""4. Muscle car")
+    print("\n""5. Limosine")
+
+    choicecar = int(input("\n""\n""Enter the number of car you have chosen : "))
+
+    if choicecar == 1:
+        print("\n""You have successfully chosen SUV! Please, wait our driver will arrive soon.")
+    elif choicecar == 2:
+        print("\n""You have successfully chosen Crossover! Please, wait our driver will arrive soon.")    
+    elif choicecar == 3:
+        print("\n""You have successfully chosen Coupe! Please, wait our driver will arrive soon.")
+    elif choicecar == 4:
+        print("\n""You have successfully chosen Muscle car! Please, wait our driver will arrive soon.")
+    else:
+        print("\n""You have successfully chosen Limousine! Please, wait our driver will arrive soon.")
+
+elif choice == 2:
+    print("\n""\n""Select a type of bike :- ")    
+    print("\n""1. Standard bike")
+    print("\n""2. Srcamble")
+    print("\n""3. Motor Trike")
+    print("\n""4. Scooter")
+    print("\n""5. Cruiser")
+
+    choicebike = int(input("\n""\n""Enter the number of bike you have chosen : "))
+
+    if choicebike == 1:
+        print("\n""You have successfully chosen Standard bike! Please, wait our driver will arrive soon.")
+    elif choicebike == 2:
+        print("\n""You have successfully chosen Srcamble! Please, wait our driver will arrive soon.")
+    elif choicebike == 3:
+        print("\n""You have successfully chosen Motor Trike! Please, wait our driver will arrive soon.")
+    elif choicebike == 4:
+        print("\n""You have successfully chosen Scooter! Please, wait our driver will arrive soon.")
+    else:
+        print("\n""You have successfully chosen Cruise! Please, wait our driver will arrive soon.")                
+
+elif choice == 3:
+    print("\n""\n""Select a type of boat :- ")  
+    print("\n""1. Air boat")   
+    print("\n""2. Jet boat")   
+    print("\n""3. Wake boat")
+    print("\n""4. Motor Yacht boat")
+    print("\n""5. Runbout")
+
+    choiceboat = int(input("\n""\n""Enter the number of boat you have chosen : "))
+
+    if choiceboat == 1:
+        print("\n""You have successfully chosen Air boat! Please, wait our driver will arive soon.")
+    elif choiceboat == 2:
+        print("\n""You have successfully chosen Jet boat! Please, wait our driver will arive soon.")
+    elif choiceboat == 3:
+        print("\n""You have successfully chosen Wake boat! Please, wait our driver will arive soon.")
+    elif choiceboat == 4:
+        print("\n""You have successfully chosen Motor Yacht boat! Please, wait our driver will arive soon.")
+    else:
+        print("\n""You have successfully chosen Runbout! Please, wait our driver will arive soon.")      
+
+else:
+    print("\n""\n""Select a type of Helicopter :- ")     
+    print("\n""1. Compound helicopter")    
+    print("\n""2. Tilt Rotor helicopter")
+    print("\n""3. Coaxial helicopter")     
+    print("\n""4. Single-Main Rotor helicopter")   
+    print("\n""5. Tandem-Rotor helicopter") 
+
+    choiceheli = int(input("\n""\n""Enter the number of helicopter you have chosen : "))
+
+    if choiceheli == 1:
+        print("\n""You have successfully chosen Compound helicopter! Please, wait our pilot will arive soon.")
+    elif choiceheli == 2:
+        print("\n""You have successfully chosen Tilt Rotor helicopter! Please, wait our pilot will arive soon.")
+    elif choiceheli == 3:
+        print("\n""You have successfully chosen Coaxial helicopter! Please, wait our pilot will arive soon.")
+    elif choiceheli == 4:
+        print("\n""You have successfully chosen Single-Main helicopter! Please, wait our pilot will arive soon.")
+    else:
+        print("\n""You have successfully chosen Tandem-Rotor helicopter! Please, wait our pilot will arive soon.")                
